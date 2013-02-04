a61c44040d76e582cd09ef9d87fcf976baee7030
ErikTrapman <veggatron@gmail.com>
2013-02-04 22:56:04 +0100

    issue #2 WIP

--- Event/CommandRunEvent.php
+++ Event/CommandRunEvent.php
@@ -14,17 +14,24 @@ class CommandRunEvent extends Event
     public function __construct(Command $command, $options)
     {
         $this->command = $command;
-        
+
         $executableOptions = array();
-        if(strlen($options) > 0 ){
-            foreach(explode(" ",$options) as $option){
-                if(!$option){continue;}
-                $executableOptions[$option] = true;
+        if (strlen($options) > 0) {
+            foreach (explode(" ", $options) as $option) {
+                if (!$option) {
+                    continue;
+                }
+                if (false !== strpos($option, "=")) {
+                    $optionParts = explode("=",$option);
+                    $executableOptions[$optionParts[0]] = $optionParts[1];
+                } else {
+                    $executableOptions[$option] = true;
+                }
             }
         }
         $this->options = $executableOptions;
     }
-    
+
     /**
      * 
      * @return \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand
@@ -38,7 +45,4 @@ class CommandRunEvent extends Event
     {
         return $this->options;
     }
-
-
-    
 }
--- EventListener/CommandRunListener.php
+++ EventListener/CommandRunListener.php
@@ -22,11 +22,14 @@ class CommandRunListener implements EventSubscriberInterface
      * @var Session
      */
     private $session;
+    
+    private $kernel;
 
-    public function __construct($output, $session)
+    public function __construct($output, $session, \AppKernel $kernel)
     {
         $this->output = $output;
         $this->session = $session;
+        $this->kernel = $kernel;
     }
 
     public static function getSubscribedEvents()
@@ -36,19 +39,24 @@ class CommandRunListener implements EventSubscriberInterface
 
     public function onCommandRun(CommandRunEvent $event)
     {
-        // TODO fix cache-dir
-        // TODO fix options: enable named arguments and option-values. 
-        // TODO other option syntax is needed for Process-builder passing options
         $command = $event->getCommand();
         $options = $event->getOptions();
-
         if (function_exists('proc_open')) {
             $f = new \Symfony\Component\Process\ExecutableFinder();
             $php = $f->find('php');
-            $rootDir = 'myPath/To/app';
+            $rootDir = $this->kernel->getRootDir();
             $processOptions = '';
             if (!empty($options)) {
-                $processOptions = implode(" ", array_keys($options));
+                $processOptions = '';
+                // Hmmm....
+                foreach($options as $name => $option){
+                    if(true === $option){
+                        $processOptions .= ' '.$name.' ';
+                    } else {
+                        $processOptions .= ' '.$option.' ';//.$option.' ';
+                    }
+                }
+                // TODO doesn't work yet...?!
                 $pb = \Symfony\Component\Process\ProcessBuilder::create(array($php, $rootDir.'/console', $command->getName(), $processOptions));
             } else {
                 $pb = \Symfony\Component\Process\ProcessBuilder::create(array($php, $rootDir.'/console', $command->getName()));
