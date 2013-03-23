70b05b0a221f6f1092685dd417c4f6e2707bd8d6
Erik Trapman <veggatron@gmail.com>
2013-03-23 17:43:15 +0100

    troubles with session. keep the command as non-symfony as possible

--- Command/CacheRemoveCommand.php
+++ Command/CacheRemoveCommand.php
@@ -31,9 +31,40 @@ class CacheRemoveCommand extends ContainerAwareCommand
             $output->write("Please configure a base_url in your config:\n\nerik_trapman_web_command:\n&nbsp;&nbsp;base_url: %base_url%");
             return;
         }
-        $fileSystem = $container->get('filesystem');
-        $fileSystem->remove($container->getParameter('kernel.cache_dir'));
+        $this->cc($container->getParameter('kernel.cache_dir'));
         // build up the cache again
         file_get_contents($baseUrl);
+    }
+    
+    
+    /**
+     * Inspired by https://gist.github.com/1942649 but this could be any recursive delete script
+     * 
+     * @see https://gist.github.com/1942649
+     */
+    private function cc($cache_dir)
+    {
+        if (is_dir($cache_dir)) {
+            $this->rrmdir($cache_dir);
+        }
+    }
+    
+    private function rrmdir($dir)
+    {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    $o = $dir."/".$object;
+                    if (filetype($o) == "dir") {
+                        $this->rrmdir($dir."/".$object);
+                    } else {
+                        unlink($o);
+                    }
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
     }
 }
